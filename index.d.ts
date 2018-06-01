ef00225d24185b2e35534b9d788edcf907c5c96c
willkan <wanghuikan@gmail.com>
2018-06-01 15:21:01 +0800

    fix index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -9,7 +9,7 @@ declare namespace onlylog {
     error(message?: any, ...optionalParams: any[]): void;
     info(message?: any, ...optionalParams: any[]): void;
     debug(message?: any, ...optionalParams: any[]): void;
-    warn?(message?: any, ...optionalParams: any[]): void;
+    warn(message?: any, ...optionalParams: any[]): void;
     on?(msg: string, handler: Function): any
   }
   export interface OnlylogOptions {
